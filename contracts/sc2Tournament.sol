067191e8570c034e10f1e28c8bd14a24736e36da
Keuwa <vincent.durpoix@hotmail.fr>
2018-02-16 13:17:42 +0100

    contract v1

--- contracts/sc2Tournament.sol
+++ contracts/sc2Tournament.sol
@@ -5,17 +5,18 @@ contract Sc2Tournaments {
   struct Tournament {
     uint id;
     string name;
-	int cashprize;
-    int registrationFees;
-	int maxPeople;
-    Player[] players;
-    Match[] matches;
+	uint cashprize;
+    uint registrationFees;
+	uint maxPeople;
+	uint actualPeople;
+    mapping (uint => Player) players;
+    mapping (uint => Match) matches;
   }
 
   struct Player {
       string name;
       string race;
-      //address playerAddress;
+      address playerAddress;
   }
 
   struct Match {
@@ -26,7 +27,10 @@ contract Sc2Tournaments {
     int round; //round of 32(16eme final), round of 16(8eme de final) etc...
   }
 
+  uint tournamentsCount = 0;
   mapping(address => uint) public balances;
+  mapping (uint => Tournament) public tournaments;
+
 
 
   event withdraw(address from, address to, uint amount);
@@ -38,31 +42,21 @@ contract Sc2Tournaments {
   }
 
 
-  function CreateTournament(string name, int registrationFees, int maxPeople) public{
+  function CreateTournament(string name, uint registrationFees, uint maxPeople) public{
     //TODO Check if tournament name doesn't exists
-    Player[]  players;
-    Match[]  matches;
 
-    /*tournaments.push(Tournament({
-        id:3 ,
-        name:name,
-        cashprize:4,
-        registrationFees:registrationFees,
-        maxPeople:maxPeople,
-        players:players,
-        matches:matches
-        }));*/
-    tournaments.push(Tournament(3,name,4,3,3,players,matches));
+    tournaments[tournamentsCount++].name = name;
+    tournaments[tournamentsCount++].cashprize = registrationFees*maxPeople;
+    tournaments[tournamentsCount++].registrationFees = registrationFees;
+    tournaments[tournamentsCount++].maxPeople = maxPeople;
+
   }
 
-  function CreateMatch(string player1Name, string player2name,int round,uint tournamentId)public{
-    tournaments[tournamentId].matches.push(Match({
-      id:tournaments[tournamentId].matches.length,
-      player1:player1Name,
-      player2:player2name,
-      winner:0,
-      round:round
-      }));
+  function CreateMatch(uint id,string player1Name, string player2Name,int round,uint tournamentId)public{
+    tournaments[tournamentId].matches[id].player1 = player1Name;
+    tournaments[tournamentId].matches[id].player2 = player2Name;
+    tournaments[tournamentId].matches[id].round = round;
+
   }
 
   function MatchResult(int winner, uint tournamentId,uint matchId )public{
@@ -71,22 +65,29 @@ contract Sc2Tournaments {
 
   }
 
-    function Inscription(string playerName, string race, uint fees) payable {
+    function Inscription(string playerName, string race, uint fees,uint tournamentId) payable {
         var _playerName = playerName;
         require(balances[msg.sender] > fees);
 
         //safes.push(Safe({owner: msg.sender, fees))
-        balances[msg.sender]+=fees;
-        cashprize += fees;
+        balances[msg.sender]-=fees;
+        //cashprize += fees;
+
+        require(tournaments[tournamentId].actualPeople < tournaments[tournamentId].maxPeople);
+        uint id = tournaments[tournamentId].actualPeople++;
+
+        tournaments[tournamentId].players[id].race = race;
+        tournaments[tournamentId].players[id].name = playerName;
+
     }
 
-    function Withdraw(Player player, uint idTournoi) payable{
+    /*function Withdraw(address playerA, uint idTournoi) public payable{
         require(balances[msg.sender] < tournaments[idTournoi].cashprize);
         require(msg.sender != owner);
 
         balances[msg.sender] -= tournaments[idTournoi].cashprize;
         player.playerAddress += tournaments[idTournoi].cashprize;
         withdraw(msg.sender, player.playerAddress, tournaments[idTournoi].cashprize);
-    }
+    }*/
 
 }
